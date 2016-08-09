4ec04f41790916762119e5ed0c58f3fe3ad2fdbf
Anthony Bretaudeau <anthony.bretaudeau@inria.fr>
2016-08-09 10:38:49 +0200

    fix

--- scripts/create_analysis.py
+++ scripts/create_analysis.py
@@ -42,5 +42,5 @@ if __name__ == '__main__':
     newa.sourceversion = args.source_version
     newa.sourceuri = args.source_uri
     newa.timeexecuted = date
-    session.add(newa)
-    session.commit()
+    ci.session.add(newa)
+    ci.session.commit()
