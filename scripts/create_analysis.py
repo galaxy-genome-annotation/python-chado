71af64dc3dc045db09dfdcecdf0bd5ec0ac6c6f7
Anthony Bretaudeau <anthony.bretaudeau@inria.fr>
2016-08-09 10:37:45 +0200

    refactor + timeexecuted

--- scripts/create_analysis.py
+++ scripts/create_analysis.py
@@ -1,6 +1,7 @@
 #!/usr/bin/env python
 import json
 import argparse
+from datetime import datetime
 from chado import ChadoAuth, ChadoInstance, Analysis
 
 if __name__ == '__main__':
@@ -13,6 +14,7 @@ if __name__ == '__main__':
     parser.add_argument("--source-name", required=True, help="Source name")
     parser.add_argument("--source-version", help="Source version")
     parser.add_argument("--source-uri", help="Source URI")
+    parser.add_argument("--date-executed", help="Date of execution of the analysis (format=YYYY-MM-DD, default=today)")
 
     ChadoAuth(parser)
     args = parser.parse_args()
@@ -23,19 +25,22 @@ if __name__ == '__main__':
 
     # check if the analysis exists
     res = ci.session.query(Analysis).filter_by(name = args.name)
-    found_res = (res.count() > 0)
 
-    if not found_res:
-        newa = Analysis()
-        newa.name = args.name
-        newa.program = args.program
-        newa.programversion = args.program_version
-        newa.algorithm = args.algorithm
-        newa.sourcename = args.source_name
-        newa.sourceversion = args.source_version
-        newa.sourceuri = args.source_uri
-        newa.timeexecuted = args.timeexecuted # This should be a timestamp
-        session.add(newa)
-        session.commit()
-    else:
+    if res.count() > 0:
         raise Exception("Found a preexisting analysis with the same attributes in the database %s" % (self._engine.url))
+
+    date = datetime.today()
+    if args.args_executed:
+        date = datetime.strptime(args.args_executed, '%Y-%m-%d')
+
+    newa = Analysis()
+    newa.name = args.name
+    newa.program = args.program
+    newa.programversion = args.program_version
+    newa.algorithm = args.algorithm
+    newa.sourcename = args.source_name
+    newa.sourceversion = args.source_version
+    newa.sourceuri = args.source_uri
+    newa.timeexecuted = date
+    session.add(newa)
+    session.commit()
