b5d395e44c667732888e3069eee6a12d6ba1468b
Anthony Bretaudeau <anthony.bretaudeau@inria.fr>
2016-08-11 10:56:31 +0200

    new method used by python-tripal

--- chado/__init__.py
+++ chado/__init__.py
@@ -78,3 +78,10 @@ class ChadoInstance(object):
         mapper(Db, db)
         cv = Table('cv', self._metadata, autoload=True)
         mapper(Cv, cv)
+
+    def get_cvterm_id(self, type_name, cv_name):
+        res = self.session.query(Cvterm, Cv).filter(Cvterm.name == type_name, Cv.name == cv_name)
+        if not res.count():
+            raise Exception("Could not find a cvterm with name '%s' from cv ''%s' in the database %s" % (type_name, cv_name, ci._engine.url))
+
+        return res.one().Cvterm.cvterm_id
