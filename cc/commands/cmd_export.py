6dec8715e71296019db855990444dc053be801d9
E Rasche <hxr@hx42.org>
2017-06-16 21:01:45 +0000

    finish conversion

--- cc/commands/cmd_export.py
+++ cc/commands/cmd_export.py
@@ -1,6 +1,7 @@
 import click
 from cc.commands.export.export_fasta import cli as func0
 from cc.commands.export.export_gff3 import cli as func1
+from cc.commands.export.export_gbk import cli as func2
 
 @click.group()
 def cli():
@@ -8,3 +9,4 @@ def cli():
 
 cli.add_command(func0)
 cli.add_command(func1)
+cli.add_command(func2)
