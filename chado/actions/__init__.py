ed86c24c0633b2cdd0f9f0de2fa37c00f20257ae
E Rasche <hxr@hx42.org>
2017-06-19 16:18:13 +0000

    Remove old actions

--- chado/actions/__init__.py
+++
@@ -1,7 +0,0 @@
-from os.path import dirname
-import sys, pkgutil
-
-for importer, package_name, _ in pkgutil.iter_modules([dirname(__file__)]):
-    full_package_name = '%s.%s' % ('chado.actions', package_name)
-    if full_package_name not in sys.modules:
-        module = importer.find_module(package_name).load_module(full_package_name)
