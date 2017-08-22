3a8ddbadc8e525e0ca38d1c4956bf72846d917d0
Anthony Bretaudeau <anthony.bretaudeau@inria.fr>
2017-08-22 14:42:21 +0200

    2.0.1

--- setup.py
+++ setup.py
@@ -5,7 +5,7 @@ from os import listdir
 
 setup(
     name="chado",
-    version='2.0',
+    version='2.0.1',
     description="Chado library",
     author="Anthony Bretaudeau",
     author_email="anthony.bretaudeau@inra.fr",
