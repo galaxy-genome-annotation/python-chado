8e9e6925d261caf91db7c3d1c84658e82046fb7f
Anthony Bretaudeau <anthony.bretaudeau@inria.fr>
2018-03-05 11:34:12 +0100

    why does it fail with py2?

--- chakin/cli.py
+++ chakin/cli.py
@@ -123,6 +123,7 @@ def chakin(ctx, instance, verbose, path=None):
         set_global_config_path(path)
     # We abuse this, knowing that calls to one will fail.
     current_ctx = click.get_current_context()
+    print(current_ctx.invoked_subcommand)
     try:
         # TODO find a way to pass offline=True when running with -h option or without any subcommand
         ctx.gi = get_instance(instance, offline=(current_ctx.invoked_subcommand in ['init', 'util']))
