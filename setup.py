0853748385a5a0fe2be9891c76926f2a798b3374
Anthony Bretaudeau <anthony.bretaudeau@inria.fr>
2018-08-02 15:10:16 +0200

    prepare 2.1.4

--- setup.py
+++ setup.py
@@ -3,7 +3,7 @@ from setuptools import find_packages, setup
 
 setup(
     name="chado",
-    version='2.1.3',
+    version='2.1.4',
     description="Chado library",
     author="Anthony Bretaudeau",
     author_email="anthony.bretaudeau@inra.fr",
