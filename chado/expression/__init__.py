87e77ff461d8b19c7465f144efdf0dfab2480db2
mboudet <mateo.boudet@irisa.fr>
2018-10-04 13:19:26 +0000

    Added add_biomaterial

--- chado/expression/__init__.py
+++ chado/expression/__init__.py
@@ -6,6 +6,9 @@ from __future__ import division
 from __future__ import print_function
 from __future__ import unicode_literals
 
+import json
+
+
 from chado.client import Client
 
 from future import standard_library
@@ -30,7 +33,7 @@ class ExpressionClient(Client):
 
 #To test
 
-    def get_biomaterials(self, provider_id="", biomaterial_id="", organism_id="", dbxref_id=""):
+    def get_biomaterials(self, provider_id="", biomaterial_id="", organism_id="", dbxref_id="", biomaterial_name=""):
         """
         List biomaterials in the database
 
@@ -38,13 +41,17 @@ class ExpressionClient(Client):
         :param organism_id: Limit query to the selected organism
 
         :type biomaterial_id: str
-        :param biomaterial_id: Limit query to the selected biomaterial
+        :param biomaterial_id: Limit query to the selected biomaterial id
 
         :type provider_id: str
         :param provider_id: Limit query to the selected provider
 
         :type dbxref_id: str
         :param dbxref_id: Limit query to the selected ref
+
+        :type biomaterial_name: str
+        :param biomaterial_name: Limit query to the selected biomaterial name
+
 
         :rtype: list
         :return: List of biomaterials
@@ -59,6 +66,8 @@ class ExpressionClient(Client):
             res = res.filter_by(taxon_id=organism_id)
         if dbxref_id:
             res = res.filter_by(dbxref_id=dbxref_id)
+        if biomaterial_name:
+            res = res.filter_by(name=biomaterial_name)
 
         data = []
         for biomat in res:
@@ -74,41 +83,211 @@ class ExpressionClient(Client):
 
 
 # WIP
-    def add_biomaterial(self, organism_id, biomaterial_name, organism_name="", provider="", accession="", sra_accession="", bioproject_accession="", attributes={}):
+    def add_biomaterial(self, biomaterial_name, organism_id,
+                        description="", analysis_id="", provider="", biosample_accession="", sra_accession="",
+                        attributes={}):
         """
         Add a new biomaterial to the database
-
-        :type organism_id: str
-        :param organism_id: The id of the associated organism
 
         :type biomaterial_name: str
         :param biomaterial_name: Biomaterial name
 
-        :type organism_name: str
-        :param organism_name: Orgnaism name (can be different from the organism ID provided)
+        :type organism_id: str
+        :param organism_id: The id of the associated organism
+
+        :type description: str
+        :param description: Description of the biomaterial
+
+        :type analysis_id: str
+        :param analysis_id: Analysis ID
 
         :type provider: str
         :param provider: Biomaterial provider name
 
-        :type accession: str
-        :param accession: Biomaterial accession
+        :type biosample_accession: str
+        :param biosample_accession: Biosample accession
 
         :type sra_accession: str
         :param sra_accession: SRA acession
 
-        :type bioproject_accession: str
-        :param bioproject_accession: Bioproject accession
-
         :type attributes: dict
         :param attributes: Custom attributes (In JSON dict form)
 
-        :rtype: dict
-        :return: Job information
+        :rtype: str
+        :return: Biomaterial id
+
         """
 
-        biomat = self.model.biomaterial()
+        biosourceprovider_id = None
+        dbxref_id = None
+
+        # Add provider into table if not existing
+        if provider:
+            biosourceprovider_id = self._create_biomaterial_contact(provider)
+
+        # Create DB if not existing. Add into dbxref table.
+
+        # Create biomaterial
+        biomaterial_id = self._create_biomaterial(biomaterial_name, analysis_id, organism_id, biosourceprovider_id, dbxref_id, description)
+
+        # Create DB for accession type, add to dbxref table, and then add to biomaterial_dbxref
+        if sra_accession:
+            dbxref_id = self._register_accession('sra', 'NCBI SRA', '', sra_accession)
+            self._add_to_biomaterial_dbxref(self, biomaterial_id, dbxref_id)
+        # Unused in the V3 version.. Mixup of accession type?
+#       if bioproject_accession:
+#            dbxref_id = self._register_accession('bioproject', 'NCBI BioProject', '', bioproject_accession)
+#            self._add_to_biomaterial_dbxref(self, biomaterial_id, dbxref_id)
+        if biosample_accession:
+            dbxref_id = self._register_accession('biosample', 'NCBI BioSample', '', biosample_accession)
+            self._add_to_biomaterial_dbxref(self, biomaterial_id, dbxref_id)
+
+        # Load attributes
+        if attributes:
+            self._add_biomaterial_props(biomaterial_id, json.load(attributes))
+
+        return biomaterial_id
 
 
+    def _register_accession(self, url_name, db_name, db_description, accession):
+        # Create DB if not existing, get id and update otherwise
+        url = "http://www.ncbi.nlm.nih.gov/"
+        urlprefix = "http://www.ncbi.nlm.nih.gov/" + url_name + "/"
+        db_id = ""
+        res = self.session.query(self.model.db).filter_by(name=db_name)
+        if res.count():
+            db_id = res.one().db_id
+        if not db_id:
+            database = self.model.db()
+            database.name = db_name
+            database.description = description
+            database.urlprefix = urlprefix
+            database.url = url
+            self.session.add(database)
+            self.session.commit()
+            db_id = database.db_id
+        # Update DB
+        else:
+            self.session.query(self.model.biomaterial).filter_by(db_id=db_id).update({
+                'name' : db_name,
+                'description' : db_description,
+                'urlprefix': urlprefix,
+                'url' : url
+            })
+            self.session.commit()
+        # Register in dbxref
 
+        dbxref_id = ""
+        res = self.session.query(self.model.dbxref).filter_by(accession=accession, db_id=db_id)
+        if res.count():
+            dbxref_id = res.one().dbxref_id
+        # Create if not existing,  no need for update
+        if not dbxref_id:
+            database = self.model.dbxref()
+            database.accession = accession
+            database.db_id = db_id
+            self.session.add(database)
+            self.session.commit()
+            dbxref_id = database.dbxref_id
 
-        return ""
+        return dbxref_id
+
+    def _create_biomaterial_contact(self, contact_name):
+
+        contact_id = ""
+        res = self.session.query(self.model.contact).filter_by(name=contact_name)
+        if res.count():
+            contact_id = res.one().contact_id
+
+        if not contact_id:
+            contact = self.model.contact()
+            contact.name = contact_name
+            self.session.add(contact)
+            self.session.commit()
+            contact_id = contact.contact_id
+
+        return contact_id
+
+    def _add_to_biomaterial_dbxref(self, biomaterial_id, dbxref_id):
+
+        biomaterial_dbxref_id = ""
+        res = self.session.query(self.model.biomaterial_dbxref).filter_by(biomaterial_id=biomaterial_id, dbxref_id=dbxref_id)
+        if res.count():
+            biomaterial_dbxref_id = res.one().biomaterial_dbxref_id
+        # Create if not existing,  no need for update
+        if not biomaterial_dbxref_id:
+            database = self.model.biomaterial_dbxref()
+            database.biomaterial_id = biomaterial_id
+            database.dbxref_id = dbxref_id
+            self.session.add(database)
+            self.session.commit()
+            biomaterial_dbxref_id = database.biomaterial_dbxref_id
+
+        return biomaterial_dbxref_id
+
+    def _create_biomaterial(self, biomaterial_name, analysis_id, organism_id, biosourceprovider_id, dbxref_id, description):
+
+        # Check if biomaterial exist
+        res_biomaterial = self.session.query(self.model.biomaterial).filter_by(name=biomaterial_name, taxon_id=organism_id)
+        biomaterial_id = ""
+        if res_biomaterial.count():
+            print("Biomaterial already exists for this organism. Will update")
+            biomaterial_id = res_biomaterial.one().biomaterial_id
+
+        if analysis_id:
+            analysis_name = ""
+            res_analysis = self.session.query(self.model.analysis).filter_by(analysis_id=analysis_id)
+            if res_analysis.count():
+                analysis_name = res_analysis.one().name
+            else:
+                print("Analysis with not found : will ignore")
+
+        if (description == "" and analysis_name):
+            description = 'This biomaterial: ' + biomaterial_name + ', was created for the analysis: ' + analysis_name
+
+        if not biomaterial_id:
+            biomat = self.model.biomaterial()
+            biomat.name = biomaterial_name
+            biomat.description = description
+            biomat.taxon_id = organism_id
+            biomat.biosourceprovider_id = biosourceprovider_id
+            biomat.dbxref_id = dbxref_id
+            self.session.add(biomat)
+            self.session.commit()
+            biomaterial_id = biomat.biomaterial_id
+        else:
+            self.session.query(self.model.biomaterial).filter_by(biomaterial_id=biomaterial_id).update({
+                'description': description,
+                'biosourceprovider_id': biosourceprovider_id,
+                'dbxref_id': dbxref_id
+            })
+            self.session.commit()
+
+        return biomaterial_id
+
+    def _add_biomaterial_props(self, biomaterial_id, prop_dict):
+
+        # Insert in Controlled Vocabulary table if not existing
+        for key, value in prop_dict:
+            try:
+                propterm = self.ci.get_cvterm_id(key, 'biomaterial_property')
+            # 'Internal DB?'
+            except chado.RecordNotFoundError:
+                propterm = self.ci.create_cvterm(key, 'biomaterial_property', 'internal')
+
+            # Cache management go here (one day)
+            # We need unicity for (biomaterial_id, term_id, rank)
+            rank = 0
+            res = self.session.query(self.model.biomaterialprop).filter_by(biomaterial_id=biomaterial_id, term_id=propterm)
+            # Make sure to increment the rank if existing
+            if res.count():
+                rank = len(res)
+            # Add to DB
+            prop = self.model.biomaterialprop()
+            prop.term_id=propterm
+            prop.biomaterial_id=biomaterial_id
+            prop.value = value
+            prop.rank = rank
+            prop.session.add(prop)
+
+        self.session.commit()
--- chakin/commands/expression/add_biomaterial.py
+++ chakin/commands/expression/add_biomaterial.py
@@ -1,14 +1,19 @@
 import click
 from chakin.cli import pass_context
-from chakin.decorators import custom_exception, dict_output
+from chakin.decorators import custom_exception, str_output
 
 
 @click.command('add_biomaterial')
-@click.argument("organism_id", type=str)
 @click.argument("biomaterial_name", type=str)
+@click.argument("organism_id", type=str)
 @click.option(
-    "--organism_name",
-    help="Orgnaism name (can be different from the organism ID provided)",
+    "--description",
+    help="Description of the biomaterial",
+    type=str
+)
+@click.option(
+    "--analysis_id",
+    help="Analysis ID",
     type=str
 )
 @click.option(
@@ -17,18 +22,13 @@ from chakin.decorators import custom_exception, dict_output
     type=str
 )
 @click.option(
-    "--accession",
-    help="Biomaterial accession",
+    "--biosample_accession",
+    help="Biosample accession",
     type=str
 )
 @click.option(
     "--sra_accession",
     help="SRA acession",
-    type=str
-)
-@click.option(
-    "--bioproject_accession",
-    help="Bioproject accession",
     type=str
 )
 @click.option(
@@ -38,12 +38,12 @@ from chakin.decorators import custom_exception, dict_output
 )
 @pass_context
 @custom_exception
-@dict_output
-def cli(ctx, organism_id, biomaterial_name, organism_name="", provider="", accession="", sra_accession="", bioproject_accession="", attributes={}):
+@str_output
+def cli(ctx, biomaterial_name, organism_id, description="", analysis_id="", provider="", biosample_accession="", sra_accession="", attributes={}):
     """Add a new biomaterial to the database
 
 Output:
 
-    Job information
+    Biomaterial id
     """
-    return ctx.gi.expression.add_biomaterial(organism_id, biomaterial_name, organism_name=organism_name, provider=provider, accession=accession, sra_accession=sra_accession, bioproject_accession=bioproject_accession, attributes=attributes)
+    return ctx.gi.expression.add_biomaterial(biomaterial_name, organism_id, description=description, analysis_id=analysis_id, provider=provider, biosample_accession=biosample_accession, sra_accession=sra_accession, attributes=attributes)
