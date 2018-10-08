4913d1897912e571ca4a18495a233026b6c3724c
mboudet <mateo.boudet@irisa.fr>
2018-10-08 13:06:35 +0000

    Fixes + cleaning up

--- chakin/commands/expression/add_biomaterial.py
+++ chakin/commands/expression/add_biomaterial.py
@@ -17,18 +17,23 @@ from chakin.decorators import custom_exception, str_output
     type=str
 )
 @click.option(
-    "--provider",
+    "--biomaterial_provider",
     help="Biomaterial provider name",
     type=str
 )
 @click.option(
     "--biosample_accession",
-    help="Biosample accession",
+    help="Biosample accession number",
     type=str
 )
 @click.option(
     "--sra_accession",
-    help="SRA acession",
+    help="SRA accession number",
+    type=str
+)
+@click.option(
+    "--bioproject_accession",
+    help="Bioproject accession number",
     type=str
 )
 @click.option(
@@ -39,11 +44,11 @@ from chakin.decorators import custom_exception, str_output
 @pass_context
 @custom_exception
 @str_output
-def cli(ctx, biomaterial_name, organism_id, description="", analysis_id="", provider="", biosample_accession="", sra_accession="", attributes={}):
+def cli(ctx, biomaterial_name, organism_id, description="", analysis_id="", biomaterial_provider="", biosample_accession="", sra_accession="", bioproject_accession="", attributes={}):
     """Add a new biomaterial to the database
 
 Output:
 
-    Biomaterial id
+    Biomaterial
     """
-    return ctx.gi.expression.add_biomaterial(biomaterial_name, organism_id, description=description, analysis_id=analysis_id, provider=provider, biosample_accession=biosample_accession, sra_accession=sra_accession, attributes=attributes)
+    return ctx.gi.expression.add_biomaterial(biomaterial_name, organism_id, description=description, analysis_id=analysis_id, biomaterial_provider=biomaterial_provider, biosample_accession=biosample_accession, sra_accession=sra_accession, bioproject_accession=bioproject_accession, attributes=attributes)
