7052e924e6e01646b023987af00995e36f090e6c
mboudet <mateo.boudet@irisa.fr>
2019-06-04 08:49:12 +0000

    chakin

--- chakin/commands/cmd_analysis.py
+++ chakin/commands/cmd_analysis.py
@@ -2,6 +2,7 @@ import click
 from chakin.commands.analysis.add_analysis import cli as func0
 from chakin.commands.analysis.delete_analyses import cli as func1
 from chakin.commands.analysis.get_analyses import cli as func2
+from chakin.commands.analysis.load_blast import cli as func3
 
 
 @click.group()
@@ -15,3 +16,4 @@ def cli():
 cli.add_command(func0)
 cli.add_command(func1)
 cli.add_command(func2)
+cli.add_command(func3)
