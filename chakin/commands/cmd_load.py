f1d0d4d639e40709ead3473e9bbcd6e9b3f3359b
mboudet <mateo.boudet@irisa.fr>
2019-06-19 07:48:47 +0000

    Chakin

--- chakin/commands/cmd_load.py
+++ chakin/commands/cmd_load.py
@@ -1,6 +1,7 @@
 import click
 from chakin.commands.load.blast import cli as func0
 from chakin.commands.load.go import cli as func1
+from chakin.commands.load.interpro import cli as func2
 
 
 @click.group()
@@ -10,3 +11,4 @@ def cli():
 
 cli.add_command(func0)
 cli.add_command(func1)
+cli.add_command(func2)
